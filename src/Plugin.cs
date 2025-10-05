6cbb2c1938fdd41b9cd68046da3dd4c4b9734b40
Mackie Kannard-Smith <floralyveltalgriffin@gmail.com>
2025-10-05 13:53:42 -0400

    Add everything

---
+++ src/Plugin.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using System;
+using System.Reflection;
+using System.Collections;
+using UnityEngine;
+
+[BepInPlugin("com.yourname.lodmod", "LOD Mod", "1.0.0")]
+public class LODMod : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        StartCoroutine(SetLODWithDelay());
+    }
+
+    private IEnumerator SetLODWithDelay()
+    {
+        // Wait for 18 seconds to ensure Unity is fully initialized
+        yield return new WaitForSeconds(18f);
+
+        try
+        {
+            Type qualitySettingsType = Type.GetType("UnityEngine.QualitySettings, UnityEngine.CoreModule");
+
+            var lodBiasProperty = qualitySettingsType?.GetProperty("lodBias");
+            lodBiasProperty?.SetValue(null, 15000f, null);
+            Logger.LogInfo("\u001b[31mLOD set to 15000!\u001b[0m");
+
+        }
+        catch (Exception e)
+        {
+            Logger.LogError($"Error: {e}");
+        }
+    }
+}
---
+++ src/obj/Debug/net46/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace BRC_LODMod
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "BRC_LODMod";
+        public const string PLUGIN_NAME = "BRC_LODMod";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
